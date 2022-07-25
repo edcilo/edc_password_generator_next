b4b7a6593a1e861c67c4dcdd67ca9196ecaefbca
edcilo <edcilop@gmail.com>
2022-07-24 20:48:47 -0500

    add google adsense

--- pages/_app.js
+++ pages/_app.js
@@ -4,10 +4,17 @@ import 'antd/dist/antd.css'
 import Script from 'next/script'
 
 function MyApp({ Component, pageProps }) {
-  const ga_url = `https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`
+  const ad_client = process.env.NEXT_PUBLIC_AD_CLIENT
+  const ga_id = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS
+  const ga_url = `https://www.googletagmanager.com/gtag/js?id=${ga_id}`
 
   return (
     <>
+      <Script 
+        data-ad-client={ad_client} 
+        async 
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js" />
+
       <Script 
         strategy="lazyOnload" 
         src={ga_url} />
@@ -17,7 +24,7 @@ function MyApp({ Component, pageProps }) {
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+          gtag('config', '${ga_id}', {
             page_path: window.location.pathname,
           });
         `}
