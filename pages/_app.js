770d36eb5fe55431218654e9c6727c84dceda091
edcilo <edcilop@gmail.com>
2022-08-02 01:26:35 -0500

    comment google ads

--- pages/_app.js
+++ pages/_app.js
@@ -4,20 +4,19 @@ import "antd/dist/antd.css"
 import Script from "next/script"
 
 function MyApp({ Component, pageProps }) {
-  const adClient = process.env.NEXT_PUBLIC_AD_CLIENT
+  // const adClient = process.env.NEXT_PUBLIC_AD_CLIENT
   const gaId = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS
   const gaUrl = `https://www.googletagmanager.com/gtag/js?id=${gaId}`
 
   return (
     <>
-      <Script
+      {/* <Script
         data-ad-client={adClient}
         async
         src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-      />
+      /> */}
 
       <Script strategy="lazyOnload" src={gaUrl} />
-
       <Script strategy="lazyOnload" id="google-analytics">
         {`
           window.dataLayer = window.dataLayer || [];
