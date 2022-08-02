0c2ed2cb2fa7d1b541a751f906c2b890583be1be
edcilo <edcilop@gmail.com>
2022-08-02 13:57:13 -0500

    listen system theme

--- components/themeCtrl/index.js
+++ components/themeCtrl/index.js
@@ -1,15 +1,33 @@
 import Icon from "@ant-design/icons"
 import { Switch } from "antd"
+import { useEffect, useState } from "react"
 
 import sun from "./sun"
 import moon from "./moon"
 
 const EdcThemeCtrl = ({ onChange }) => {
+  const [theme, setTheme] = useState(false)
+
+  const changeHandler = (checked) => {
+    setTheme(checked ? "dark" : "light")
+  }
+
+  useEffect(() => {
+    onChange(theme)
+  }, [theme])
+
+  useEffect(() => {
+    const event = window.matchMedia("(prefers-color-scheme: dark)")
+    event.addEventListener("change", (e) => changeHandler(e.matches))
+    changeHandler(event.matches)
+  }, [])
+
   return (
     <Switch
+      checked={theme === "dark"}
       checkedChildren={<Icon component={moon} />}
       unCheckedChildren={<Icon component={sun} />}
-      onChange={(checked) => onChange(checked ? "dark" : "light")}
+      onChange={changeHandler}
     />
   )
 }
