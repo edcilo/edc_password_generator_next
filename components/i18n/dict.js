8b3ac98d9147db664ee24cacb355991569c045b5
edcilo <edcilop@gmail.com>
2022-08-10 18:00:52 -0500

    fix typo

--- components/i18n/dict.js
+++ components/i18n/dict.js
@@ -5,7 +5,7 @@ const i18n = rosetta({
     title: "Password Generator",
     subtitle: "Generate a secure password!",
     disclaimer:
-      "This tool work on the client side. The passwords you generate are never sent across the web. Please be careful where you paste them.",
+      "This tool works on the client side. The passwords you generate are never sent across the web. Please be careful where you paste them.",
     btnCopy: "Click to copy!",
     btnNewPassword: "New password",
     charLengthCtrl: "Password length",
