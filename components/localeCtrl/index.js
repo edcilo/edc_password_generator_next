657721bb25d0464950ab9d03e72f9c76e0d46add
edcilo <edcilop@gmail.com>
2023-02-20 10:08:02 -0600

    fix(locale control): update menu

--- components/localeCtrl/index.js
+++ components/localeCtrl/index.js
@@ -1,36 +1,32 @@
 import { TranslationOutlined } from "@ant-design/icons"
-import { Button, Dropdown, Menu } from "antd"
+import { Button, Dropdown } from "antd"
 import { useRouter } from "next/router"
 import Link from "next/link"
 
 const EdcLocaleCtrl = ({ ...props }) => {
   const router = useRouter()
 
-  const menu = (
-    <Menu
-      items={[
-        {
-          key: "en",
-          label: (
-            <Link aria-label="English locale" role="link" href="/" locale="en">
-              English
-            </Link>
-          ),
-        },
-        {
-          key: "es",
-          label: (
-            <Link aria-label="Spanish locale" role="link" href="/" locale="es">
-              Español
-            </Link>
-          ),
-        },
-      ]}
-    />
-  )
+  const items = [
+    {
+      key: "en",
+      label: (
+        <Link aria-label="English locale" role="link" href="/" locale="en">
+          English
+        </Link>
+      ),
+    },
+    {
+      key: "es",
+      label: (
+        <Link aria-label="Spanish locale" role="link" href="/" locale="es">
+          Español
+        </Link>
+      ),
+    },
+  ]
 
   return (
-    <Dropdown overlay={menu} placement="bottomRight">
+    <Dropdown menu={{ items }} placement="bottomRight">
       <Button
         {...props}
         aria-label="change locale menu"
